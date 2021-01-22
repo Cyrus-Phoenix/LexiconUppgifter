ea205f1902534a2c5c0a3a5a25fedd64ad089902
Reza <MReza.Shahbazi@live.com>
2021-01-22 09:20:51 +0100

    Update

--- Uppgift16.cs
+++ Uppgift16.cs
@@ -3,30 +3,58 @@
 
 namespace LexiconUppgifter
 {
+    /* 
+     * Funktion där användaren ska ange namnet på sin karaktär och namnet på en motståndare. 
+     * Funktionen skall sedan själv lägga till slumpmässiga värden för Hälsa, Styrka och Tur, som sparas i en instans av en klass.
+     */
+    class Player
+    {
+        
+        public string name;
+        public int  health;
+        public int  luck;
+        public int  strength;
+
+    }
+
+
+
     class Uppgift16
     {
         public void Upgft16()
         {
+            Console.WriteLine("========================== VÄLKOMMEN TILL UPPGIFT 16 ==========================");
             while (true)
             {
 
+                Random rnd = new Random();
+                Console.WriteLine("Vad är din karaktärs namn? ");
+                Player player = new Player();
+                player.name = Console.ReadLine();
+                player.health = rnd.Next(50, 100);
+                player.luck = rnd.Next(1, 5);
+                player.strength = rnd.Next(100,500);
+
+
+                Console.WriteLine("Vad är din motståndares namn? ");
+                Player enemy = new Player();
+                enemy.name = Console.ReadLine();
+                enemy.health = rnd.Next(50, 100);
+                enemy.luck = rnd.Next(1, 5);
+                enemy.strength = rnd.Next(100, 500);
 
 
 
-
-
-
-
-
-
-
-
+                Console.WriteLine("Spelarens\nNamn: "+ player.name + "\nStyrka: "+player.strength +"\nLiv: "+player.health+"\nTur: " + player.luck);
+                Console.WriteLine("");
+                Console.WriteLine("Motståndarens\nNamn: "+ enemy.name + "\nStyrka: "+enemy.strength +"\nLiv: "+enemy.health+"\nTur: " + enemy.luck);
 
 
                 //Tillbaka till listan
                 Console.WriteLine("");
                 Console.Write("skriv 'tbx' för att återvända till val av uppgift ( annars klicka på 'enter' på tangentbordet för att köra uppgiften igen ): ");
                 Console.WriteLine("");
+
                 var tillbaka = Console.ReadLine();
                 if (tillbaka == "tbx")
                     break;
