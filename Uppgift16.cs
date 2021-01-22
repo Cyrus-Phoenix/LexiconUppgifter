c414880c30783b0b498e2c3714f49d79cc78c807
Reza <MReza.Shahbazi@live.com>
2021-01-22 03:19:01 +0100

    Ordnade loopen för uppgift 16.

--- Uppgift16.cs
+++ Uppgift16.cs
@@ -7,6 +7,8 @@ namespace LexiconUppgifter
     {
         public void Upgft16()
         {
+            while (true)
+            {
 
 
 
@@ -18,9 +20,18 @@ namespace LexiconUppgifter
 
 
 
-            Console.WriteLine("");
-            Console.Write("skriv 'tbx' för att återvända till val av uppgift ( annars klicka på 'enter' på tangentbordet för att köra uppgiften igen ): ");
-            Console.WriteLine("");
+
+
+
+                //Tillbaka till listan
+                Console.WriteLine("");
+                Console.Write("skriv 'tbx' för att återvända till val av uppgift ( annars klicka på 'enter' på tangentbordet för att köra uppgiften igen ): ");
+                Console.WriteLine("");
+                var tillbaka = Console.ReadLine();
+                if (tillbaka == "tbx")
+                    break;
+            }
+
         }
     }
 }
